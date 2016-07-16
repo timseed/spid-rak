34cb72303abca3d120fb4e242c9a791e1b80ba17
Tim Seed <tim@sy-edm.com>
2016-07-16 10:55:22 +0400

    V0.1 changes

--- ham_dev/rotator/spid3.py
+++ ham_dev/rotator/spid3.py
@@ -1,44 +1,48 @@
- '''
- This is a test program for the Alfaspid RAK controller and
- is written in Python 2.7
-
- The program was written for demonstration purposes only and as
- a template for users to fashion any custom software project
- they may be attempting.
-
- Before using this program, the user must:
-   1.  Install python2 followed by the pyserial module on the computer to
-       be used.
-   2.  Install and set-up the RAK Rotator and Controller in accordance
-       with the Alfaspid RAK Manual and ensure that it is working with the
-       manual controls on the controller.
-   3.  Obtain a controller program such as Ham Radio Deluxe or N1MM to
-       confirm that the RS232 or USB connection between the Computer
-       and Controller are fully functional.
-
- Obtain a copy of file "Program_format-Komunicacji-2005-08-10.pdf"
- from the Alfaradio website to fully understand this program.
-
- This program was developed on a DELL 610 Laptop with Windows XP and
- tested on a computers running Windows 7, Debian Linux and OSX 10.10
-
- No warranty is stated nor implied by Alfaradio for this program's use.
-
-
-This module was converted to Python3 by Tim Seed a45wg@sy-edm.com
-There are no known issues in using this module
-
-
-Please note The SPID rotator uses 1200 Baud !!
-
-Working:
-    MoveTo
-    Status
-
-ToDo:
-   check stop functions
-
 '''
+     This is a test program for the Alfaspid RAK controller and
+     is written in Python 2.7
+
+     The program was written for demonstration purposes only and as
+     a template for users to fashion any custom software project
+     they may be attempting.
+
+     Before using this program, the user must:
+       1.  Install python2 followed by the pyserial module on the computer to
+           be used.
+       2.  Install and set-up the RAK Rotator and Controller in accordance
+           with the Alfaspid RAK Manual and ensure that it is working with the
+           manual controls on the controller.
+       3.  Obtain a controller program such as Ham Radio Deluxe or N1MM to
+           confirm that the RS232 or USB connection between the Computer
+           and Controller are fully functional.
+
+     Obtain a copy of file "Program_format-Komunicacji-2005-08-10.pdf"
+     from the Alfaradio website to fully understand this program.
+
+     This program was developed on a DELL 610 Laptop with Windows XP and
+     tested on a computers running Windows 7, Debian Linux and OSX 10.10
+
+     No warranty is stated nor implied by Alfaradio for this program's use.
+
+
+    This module was converted to Python3 by Tim Seed a45wg@sy-edm.com
+    There are no known issues in using this module
+
+
+    Please note The SPID rotator uses 1200 Baud !!
+
+    Working:
+        MoveTo
+        Status
+        stop
+
+    Internal Methods
+        dump
+        hex_dump
+
+    All Method are documented.
+'''
+
 
 from time import sleep
 import logging
@@ -46,10 +50,7 @@ from .spid_serial3 import spid_serial3
 
 class spid3(object):
     '''
-    get the Comm Port information
-    input_variable = raw_input ("Enter comm port: (Default /dev/cu.usbserial-A104FZJ8 ")
-    if len(input_variable) < 5:
-    input_variable="/dev/cu.usbserial-A104FZJ8"
+    A Python3 compatable Spid Rotator class
     '''
 
     def __init__(self, port="/dev/tty.usbserial-A104FZJ8", speed=1200, timeout=10):
@@ -82,6 +83,33 @@ class spid3(object):
         except Exception as err:
             self.logger.error(str.format("Err: {} {} Unable to open device {} ", str(err),type(err),port))
 
+    def hex_dump(self,data):
+        '''
+        Used for Checking the data recevied
+        :param data:
+        :return:
+        '''
+        hex_data=":".join("{:02x}".format(ord(c)) for c in data)
+        return hex_data
+
+    def decode(self,data):
+        '''
+        Convert encoded data from the controller into degrees.
+
+        return -1 if there is an error
+
+        :param data: Hex Encoded Data. i.e.  'W\x06\x07\x02 ' which is 672 but then mod 360 > 312
+        :return: heading of rotator 0-360 is ok -1 means error.
+        '''
+        try:
+            if len(self.hex_dump(data))>12:
+                azs=(ord(data[1])*100+ord(data[2])*10+ord(data[3]))%360
+                return azs
+            else:
+                return -1
+        except:
+            return -1
+
     def stop(self):
         '''
         Stop the Movement of the Rotator
@@ -91,11 +119,16 @@ class spid3(object):
         x = self.ser.write(out)
         # Wait for answer from controller
         sleep(0.5)
-        azs=self.status()
+        data = self.ser.read(5)
+        azs=self.decode(data)
         print(("Rotator currently at %3d " % (azs) + "Degrees"))
-            return azs
+        return azs
 
     def status(self):
+        '''
+        Get current azimuth
+        :return: heading of rotator in degress as an int
+        '''
         # Build the status command word.
         out = chr(87) + self.zero5 + self.zero5 + chr(31) + chr(32)
         x = self.ser.write(out)
@@ -105,31 +138,31 @@ class spid3(object):
         data = self.ser.read(5)
         # once all 5 characters are received, decode location.
         if len(data) >= 5:
-            s1 = ord(data[1].encode('latin-1'))
-            s2 = ord(data[2].encode('latin-1'))
-            s3 = ord(data[3].encode('latin-1'))
-            azs = s1 * 100 + s2 * 10 + s3
-            # Since the controller sends the status based on 0 degrees = 360
-            # remove the 360 here
-            azs = azs % 360
+            azs=self.decode(data)
             print(("Rotator currently at %3d " % (azs) + "Degrees"))
             return azs
 
     def moveto(self, az):
-            # send command to rotator controller to move rotator
-            # to the desired azimuth.
-            az=int(az)
-            # test to see if azimuth is in the range of 0 to 360 Degrees
-            if (az < 0 or az > 360):
-                self.logger.error("Invalid Azimuth")
-                return
-            else:
-                # Convert Azimuth to number required by controller
-                az = az + 360
-                # Build message to be sent to controller
-                out = chr(87) + str(az) + chr(48) + chr(1) + self.zero5 + chr(47) + chr(32)
-                # Send message to Controller
-                x = self.ser.write(out)
+        '''
+        Move the rotator to a heading
+        :param az:
+        :return: return the number of butes sent
+        '''
+
+        # send command to rotator controller to move rotator
+        # to the desired azimuth.
+        az=int(az)%360
+        # test to see if azimuth is in the range of 0 to 360 Degrees
+        if (az < 0 or az > 360):
+            self.logger.error("Invalid Azimuth")
+            return
+        else:
+            # Convert Azimuth to number required by controller
+            az = az + 360
+            # Build message to be sent to controller
+            out = chr(87) + str(az) + chr(48) + chr(1) + self.zero5 + chr(47) + chr(32)
+            # Send message to Controller
+            x = self.ser.write(out)
 
     def __del__(self):
             self.ser.flushInput()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='spid3',
+    version='v0.1',
+    packages=['ham_dev', 'ham_dev.rotator'],
+    url='https://github.com/timseed/spid-rak.git',
+    license='GNU',
+    author='Tim Seed',
+    author_email='a45wg@sy-edm.com',
+    description='SPID Rotator for Python 3.x '
+)
