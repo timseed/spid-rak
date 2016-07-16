d1eb14fcd0729b87e2442439230941df1ddd6995
Tim Seed <tim@sy-edm.com>
2016-07-16 09:59:41 +0400

    Stop method

--- ham_dev/rotator/spid3.py
+++ ham_dev/rotator/spid3.py
@@ -1,53 +1,50 @@
-# This is a test program for the Alfaspid RAK controller and
-# is written in Python 2.7
+ '''
+ This is a test program for the Alfaspid RAK controller and
+ is written in Python 2.7
 
-# The program was written for demonstration purposes only and as
-# a template for users to fashion any custom software project
-# they may be attempting.
+ The program was written for demonstration purposes only and as
+ a template for users to fashion any custom software project
+ they may be attempting.
 
-# Before using this program, the user must:
-#   1.  Install python2 followed by the pyserial module on the computer to
-#       be used.
-#   2.  Install and set-up the RAK Rotator and Controller in accordance
-#       with the Alfaspid RAK Manual and ensure that it is working with the
-#       manual controls on the controller.
-#   3.  Obtain a controller program such as Ham Radio Deluxe or N1MM to
-#       confirm that the RS232 or USB connection between the Computer
-#       and Controller are fully functional.
+ Before using this program, the user must:
+   1.  Install python2 followed by the pyserial module on the computer to
+       be used.
+   2.  Install and set-up the RAK Rotator and Controller in accordance
+       with the Alfaspid RAK Manual and ensure that it is working with the
+       manual controls on the controller.
+   3.  Obtain a controller program such as Ham Radio Deluxe or N1MM to
+       confirm that the RS232 or USB connection between the Computer
+       and Controller are fully functional.
 
-# Obtain a copy of file "Program_format-Komunicacji-2005-08-10.pdf"
-# from the Alfaradio website to fully understand this program.
+ Obtain a copy of file "Program_format-Komunicacji-2005-08-10.pdf"
+ from the Alfaradio website to fully understand this program.
 
-# This program was developed on a DELL 610 Laptop with Windows XP and
-# tested on a computers running Windows 7, Debian Linux and OSX 10.10
+ This program was developed on a DELL 610 Laptop with Windows XP and
+ tested on a computers running Windows 7, Debian Linux and OSX 10.10
 
-# No warranty is stated nor implied by Alfaradio for this program's use.
+ No warranty is stated nor implied by Alfaradio for this program's use.
 
 
-#This module was converted to Python3 by Tim Seed a45wg@sy-edm.com
-#There are no known issues in using this module
+This module was converted to Python3 by Tim Seed a45wg@sy-edm.com
+There are no known issues in using this module
 
-'''
+
 Please note The SPID rotator uses 1200 Baud !!
 
 Working:
     MoveTo
+    Status
 
 ToDo:
-   check the status and stop functions
+   check stop functions
 
 '''
 
-# required libraries
-import serial
-import time
-import os
 from time import sleep
 import logging
 from .spid_serial3 import spid_serial3
 
 class spid3(object):
-
     '''
     get the Comm Port information
     input_variable = raw_input ("Enter comm port: (Default /dev/cu.usbserial-A104FZJ8 ")
@@ -86,22 +83,17 @@ class spid3(object):
             self.logger.error(str.format("Err: {} {} Unable to open device {} ", str(err),type(err),port))
 
     def stop(self):
+        '''
+        Stop the Movement of the Rotator
+        :return: Current Bearing in Degrees
+        '''
         out = chr(87) + self.zero5 + self.zero5 + chr(15) + chr(32)
         x = self.ser.write(out)
         # Wait for answer from controller
         sleep(0.5)
-
-        data = self.ser.read()
-        # once all 5 characters are received, decode location.
-        if len(data) >= 5:
-            s1 = ord(data[1].encode('latin-1'))
-            s2 = ord(data[2].encode('latin-1'))
-            s3 = ord(data[3].encode('latin-1'))
-            azs = s1 * 100 + s2 * 10 + s3
-            # Since the controller sends the status based on 0 degrees = 360
-            # remove the 360 here
-            azs = azs - 360
-            print(("Rotator stopped at %3d " % (azs) + "Degrees"))
+        azs=self.status()
+        print(("Rotator currently at %3d " % (azs) + "Degrees"))
+            return azs
 
     def status(self):
         # Build the status command word.
@@ -154,4 +146,3 @@ class spid3(object):
                     print("Failed to correctly close the serial Port - a machine restart will probably be needed.")
             except:
                 print('Closing ')
-
